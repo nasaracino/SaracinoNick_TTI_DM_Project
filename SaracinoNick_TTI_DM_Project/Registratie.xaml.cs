fe9800306be538cb02df3b2edba2268535a3e110
nasaracino <nasaracino@gmail.com>
2021-05-04 21:48:09 +0200

    Github repo aangemaakt and login functionaliteit klaar

---
+++ SaracinoNick_TTI_DM_Project/Registratie.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SaracinoNick_TTI_DM_Project
+{
+    /// <summary>
+    /// Interaction logic for Registratie.xaml
+    /// </summary>
+    public partial class Registratie : Window
+    {
+        public Registratie()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistreren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Udemy_DAL/DataOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_DAL
+{
+    public static class DatabaseOperations
+    {
+        public static Gebruiker OphalenGebruiker(string email, string wachtwoord)
+        {
+            using (UdemyEntities entities = new UdemyEntities())
+            {
+                var query = entities.Gebruikers
+                    .Where(x => x.email == email && x.wachtwoord == wachtwoord);
+                return query.SingleOrDefault();
+            }
+        }
+
+    }
+}
