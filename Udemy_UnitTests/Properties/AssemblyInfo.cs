e5ed319a0f8c5d395740578872e19f5382d8a1f8
nasaracino <nasaracino@gmail.com>
2021-05-17 14:04:11 +0200

    UnitTest toegevoegd

---
+++ Udemy_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Udemy_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Udemy_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("875bdcdc-45a5-42a2-8294-a07c17c86fbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Udemy_UnitTests/UdemyTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Udemy_DAL;
+
+namespace Udemy_UnitTests
+{
+    [TestClass]
+    public class UdemyTests
+    {
+        [TestMethod]
+        public void AanmakenGebruiker_IsGeldig()
+        {
+            //Arrange
+            Gebruiker gebruiker = new Gebruiker();
+            gebruiker.achternaam = "TestAchternaam";
+            gebruiker.voornaam = "TestVoornaam";
+            gebruiker.geboortedatum = new DateTime(2001, 1, 1);
+            gebruiker.email = "testemail@test.com";
+            gebruiker.geslacht = "Man";
+            gebruiker.wachtwoord = "testwachtwoord";
+
+            //Act
+            bool uitkomst = gebruiker.IsGeldig();
+
+            //Assert
+            Assert.IsTrue(uitkomst);
+
+        }
+
+        [TestMethod]
+        public void OphalenGebruiker_Test()
+        {
+
+            //Act
+            bool uitkomst;
+            Gebruiker testGebruiker = DatabaseOperations.OphalenGebruiker("janjanssens@gmail.com", "voorbeeldwachtwoord");
+            if (testGebruiker != null)
+            {
+                uitkomst = true;
+            }
+            else
+            {
+                uitkomst = false;
+            }
+
+            //Assert
+            Assert.IsTrue(uitkomst);
+        }
+    }
+}
